2a956cbd6440ac865f5597df34188da4831cd382
Praveen946-PS <praveen.931410@gmail.com>
2026-05-28 05:44:27 +0000

    commit2

--- test4.sql
+++ test4.sql
@@ -1,1 +1,10 @@
-select 1 
+USE ROLE ACCOUNTADMIN;
+
+-- Get latest from GitHub
+ALTER GIT REPOSITORY NEW_TEST.AUDIT.github_test FETCH;
+
+-- See the files
+LS @NEW_TEST.AUDIT.github_test/branches/main/;
+
+-- Read a specific file's contents
+SELECT $1 FROM @NEW_TEST.AUDIT.github_test/branches/main/test4.sql;
